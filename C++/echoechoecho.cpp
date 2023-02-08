09fc7981c3f2e3a219b78ad000d67b60bec610a6
SivarathinamSD <120994706+SivarathinamSD@users.noreply.github.com>
2023-02-08 07:25:03 +0530

    Add files via upload

---
+++ C++/echoechoecho.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string myString;
+    cin >> myString;
+    cout << myString << " "<< myString << " " << myString << endl;
+}
---
+++ C++/jackolanternjuxtaposition.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num1 , num2 ,num3;
+    cin >> num1 >> num2 >>num3;
+    int possiblities = num1 * num2 * num3;
+    cout << possiblities << endl;
+}
